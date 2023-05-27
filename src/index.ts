3c8b990b60b5af5342b06939a5358a7df9314045
Brian Ngamsom <60306190+ngamsomset@users.noreply.github.com>
2023-05-27 20:20:46 +1000

    create Eventing class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { User } from './models/User';
 
 const user = new User({ id: 1 });
-const user1 = new User({ name: 'kkk', age: 888 });
-user.set({ name: 'xxxx', age: 999 });
+user.events.on('change', () => {
+  console.log('change!');
+});
 
-user.save();
+user.events.trigger('change');
