443bdb9f42495eab6372912d2b44d3bae5eb1f96
Brian Ngamsom <60306190+ngamsomset@users.noreply.github.com>
2023-05-27 16:29:06 +1000

    add event trigger fn

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { User } from './models/User';
 
 const user = new User({ name: 'John', age: 20 });
 
-user.on('change', () => {});
+user.on('change', () => {
+  console.log('change1');
+});
+user.on('change', () => {
+  console.log('change2');
+});
 
-console.log(user);
+user.trigger('change');
