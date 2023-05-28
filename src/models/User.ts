25ef0a054eff29aa03148daad6406e5c7b66c380
Brian Ngamsom <60306190+ngamsomset@users.noreply.github.com>
2023-05-28 19:06:15 +1000

    set pass through methods in class user

--- src/models/User.ts
+++ src/models/User.ts
@@ -12,4 +12,22 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
+
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  //pass through methods, this class act as a bridge between caller and another class.
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
 }
