72893dfa2fc9891e5a9ba89586c1afba31fdc967
Brian Ngamsom <60306190+ngamsomset@users.noreply.github.com>
2023-05-28 19:06:32 +1000

    index clean up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,3 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
-user.events.on('change', () => {
-  console.log('change!');
-});
-
-user.events.trigger('change');
+const user = new User({ id: 1, name: 'test name' });
