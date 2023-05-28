23b6d7e8ec9f4673fd315918d7e1d6ea0b450c2a
Brian Ngamsom <60306190+ngamsomset@users.noreply.github.com>
2023-05-28 19:26:01 +1000

    add set method

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ id: 1, name: 'test name' });
 
-console.log(user.get('name'));
+user.on('change', () => {
+  console.log('User was changed');
+});
+
+user.set({ name: 'new name' });
