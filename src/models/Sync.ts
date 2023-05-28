01d7063d337c6857414208114158ccbbc196dc4e
Brian Ngamsom <60306190+ngamsomset@users.noreply.github.com>
2023-05-28 16:18:10 +1000

    init Sync class

---
+++ src/models/Sync.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosPromise } from 'axios';
+
+interface HasId {
+  id: number;
+}
+
+//make Sync class to be a generic class so that we can pass any type of value to this class. eg. User, students.
+export class Sync<T extends HasId> {
+  constructor(public rootUrl: string) {}
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+
+  save(data: T): AxiosPromise {
+    const { id } = data;
+
+    if (id) {
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      return axios.post('${this.rootUrl}', data);
+    }
+  }
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,5 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
-interface UserProps {
+export interface UserProps {
   //mark these as optional so that if we want to create a new instance of User we dont have to pass these in.
   id?: number;
   name?: string;
@@ -18,23 +17,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((res: AxiosResponse): void => {
-        this.set(res.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
   }
 }
